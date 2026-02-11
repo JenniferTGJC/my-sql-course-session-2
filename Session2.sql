c72b354b3f538ce88cd3695772b0ce9dc754415c
JenniferTGJC <jennwebdeveloper@gmail.com>
2026-02-11 16:13:34 +0000

    end of course

---
+++ Session2.sql
@@ -0,0 +1,62 @@
+--SELECT
+ps.patientId
+, ps.AdmittedDate
+, ps.DischargeDate
+, ps.Hospital
+
+/*
+Foundation Recap Exercise
+Use the table PatientStay.  
+This lists 44 patients admitted to London hospitals over 5 days between Feb 26th and March 2nd 2024
+*/
+SELECT
+	*
+FROM
+	PatientStay ps ;
+/*
+1. Filter the list the patients to show only those  -
+a) in the Oxleas hospital,
+b) and also in the PRUH hospital,
+c) admitted in February 2024
+d) only the surgical wards (i.e. wards ending with the word Surgery)
+
+2. Show the PatientId, AdmittedDate, DischargeDate, Hospital and Ward columns only, not all the columns.
+3. Order results by AdmittedDate (latest first) then PatientID column (high to low)
+4. Add a new column LengthOfStay which calculates the number of days that the patient stayed in hospital, inclusive of both admitted and discharge date.
+*/
+-- Write the SQL statement here
+SELECT 
+    ps.PatientId
+    ,ps.AdmittedDate
+    ,ps.DischargeDate
+    ,ps.Hospital
+    ,ps.Ward
+    ,ps.Hospital
+    ,DATEDIFF(DAY, ps.AdmittedDate, ps.DischargeDate) AS LengthOfStay --alias for column name - used to add a column name--
+FROM
+    PatientStay ps
+WHERE ps.Hospital IN ('Oxleas', 'Pruh')
+    AND ps.AdmittedDate BETWEEN '2024-02-02' AND '2024-02-29'
+    AND ps.ward LIKE '%Surgery'
+ORDER BY
+    ps.AdmittedDate DESC,
+    ps.patientId DESC
+
+5. How many patients has each hospital admitted? 
+6. How much is the total tariff for each hospital?
+7. List only those hospitals that have admitted over 10 patients
+8. Order by the hospital with most admissions first
+*/
+-- Write the SQL statement here
+ SELECT --columns are held in this space--
+    ps.Hospital
+    ,count(*) as NumberofPatients
+    ,SUM(ps.Tariff) as TotalTariff
+FROM
+    PatientStay ps 
+GROUP BY ps.Hospital
+HAVING COUNT(*) > 10
+ORDER BY NumberofPatients DESC
+
+raw.githubusercontent.com/MarkWilcock/sql-course/refs/heads/main/Foundation Recap Exercise.sql
+ 
